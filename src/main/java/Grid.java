1430094456d2b478f79871a6e1ec0ce201cbc606
Whipander <mihaja2506@gmail.com>
2025-07-16 11:31:31 +0300

    feat: create grid and generate junks

--- src/main/java/Grid.java
+++ src/main/java/Grid.java
@@ -1,2 +1,53 @@
+import lombok.*;
+
+import java.util.Arrays;
+import java.util.Random;
+
+@Getter
+@Setter
+@ToString
+@NoArgsConstructor
+@AllArgsConstructor
+
 public class Grid {
+    private char[][] grid;
+    private int gridSize;
+    private int numberOfJunks;
+
+    public Grid(int gridSize, int numberOfJunks) {
+        this.gridSize = gridSize;
+        this.numberOfJunks = numberOfJunks;
+        createGrid();
+        generateRandomnJunks();
+    }
+
+    public void createGrid() {
+        grid = new char[gridSize][gridSize];
+        for (int i = 0; i < gridSize; i++) {
+            for (int j = 0; j < gridSize; j++) {
+                grid[i][j] = '.';
+            }
+        }
+    }
+
+    public int getRandomCoordinate() {
+        Random r = new Random();
+        int low = 0;
+        int high = this.gridSize;
+        return r.nextInt(high - low) + low;
+    }
+
+    public void generateRandomnJunks(){
+        for (int i = 0; i < numberOfJunks; i++) {
+            int x = getRandomCoordinate();
+            int y = getRandomCoordinate();
+            grid[x][y] = 'J';
+        }
+    }
+
+    public static void main(String[] args) {
+        Grid grid = new Grid(5, 5);
+        System.out.println(grid);
+    }
 }
+
